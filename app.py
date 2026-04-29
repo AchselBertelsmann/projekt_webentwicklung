e9db367a3d8804b1b9bccf21d7404d300b0a5c8f
Patrick Reuter <me@patrickreuter.eu>
2026-04-29 09:28:12 +0200

    Update

--- app.py
+++ app.py
@@ -5,5 +5,6 @@ app = Flask(__name__)
 def index():
     return "Test"
 
+# App nicht nur über localhost sondern auch Netzwerk verfügbar machen (0.0.0.0)
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
