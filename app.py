9f1383f7bc4a9072c4a6f544956cb9c8e86b01ed
Patrick Reuter <me@patrickreuter.eu>
2026-04-29 11:33:50 +0200

    Bilder und Startseite hinzugefügt

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Test"
+    return render_template("index.html")
+
+@app.route("/kameras")
+def index():
+    return render_template("kameras.html")
+
+
+@app.route("/filme")
+def index():
+    return render_template("filme.html")
+
+@app.route("/objektive")
+def index():
+    return render_template("objektive.html")
 
 # App nicht nur über localhost sondern auch Netzwerk verfügbar machen (0.0.0.0)
 if __name__ == "__main__":
